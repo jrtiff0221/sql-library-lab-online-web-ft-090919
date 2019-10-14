3f2469c273ce85593bfe1fb2621393c6e66606a0
Johnnae Tiffany <jrtiff0221@gmail.com>
2019-10-14 06:17:49 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,4 +1,4 @@
-INSERT INTO series(id,title,author_id,subgenre_id) VALUES
+INSERT INTO series(id, title, author_id, subgenre_id) VALUES
 (1, "The Dark Tower", 1, 1),
 (2, "His Dark Materials", 2, 2);
 
