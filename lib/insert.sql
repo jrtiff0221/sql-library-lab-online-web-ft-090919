73b804a587f04a18c3628bf55e2db21fa0c3fa80
Johnnae Tiffany <jrtiff0221@gmail.com>
2019-10-14 06:33:41 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -28,4 +28,4 @@ INSERT INTO charactxers(id, name, motto, species, author_id) VALUES
 (7, "Lord Asriel","That is the truth", "human", 7),
 (8, "Iorek Byrnison", "I lift heavy objects", "polar bear", 8);
 
-INSERT INTO character_books (id, book_id, character_id) VALUES (1, 1, 1), (2, 1, 2), (3, 2, 2), (4, 3, 2), (5, 1, 3), (6, 2, 3), (7, 3, 3), (8, 1, 4);
+INSERT INTO character_books (id, book_id, character_id) VALUES (1, 1, 1), (2, 1, 2), (3, 1, 3), (4, 2, 4), (5, 2, 5), (6, 2, 6), (7, 2, 7), (8, 2, 8);
