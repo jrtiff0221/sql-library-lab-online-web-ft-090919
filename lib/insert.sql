c5f06753bb054a1e3b856a0a34b75233163a2311
Johnnae Tiffany <jrtiff0221@gmail.com>
2019-10-14 06:25:45 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,16 +1,16 @@
-INSERT INTO subgenres (id,name) VALUES
+INSERT INTO subgenres(id,name) VALUES
 (1, "western fiction"),
 (2, "science fantasy");
 
-INSERT INTO authors (id,name) VALUES
+INSERT INTO authors(id,name) VALUES
 (1, "Stephen King"),
 (2, "Philip Pullman");
 
-INSERT INTO series (id, title, author_id, subgenre_id) VALUES
+INSERT INTO series(id, title, author_id, subgenre_id) VALUES
 (1, "The Dark Tower", 1, 1),
 (2, "His Dark Materials", 2, 2);
 
-INSERT INTO books (id,title,year,series_id) VALUES
+INSERT INTO books(id,title,year,series_id) VALUES
 (1, "The Gunslinger", 1982, 1),
 (2, "The Drawing of the Three", 1987, 2),
 (3, "The Waste Lands", 1991, 3),
@@ -18,7 +18,7 @@ INSERT INTO books (id,title,year,series_id) VALUES
 (5, "The Subtle Knife", 1997, 5),
 (6, "The Amber Spyglass", 2000, 6);
 
-INSERT INTO characters (id, name, motto, species, author_id) VALUES
+INSERT INTO characters(id, name, motto, species, author_id) VALUES
 (1, "Roland Deschain","Time's the thief of memory", "human", 1),
 (2, "The Man in Black", "Everyone who walks with you dies", "immortal", 2),
 (3, "Eddie Dean", "You’re a Tower Junkie, Roland", "human", 3),
